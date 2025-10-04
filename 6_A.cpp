07485355898c85f5cd54310029a79c4478405266
Alisha <alishaofficial.xvi@gmail.com>
2025-10-04 18:36:04 +0530

    Add files via upload

--- 6_A.cpp
+++ 6_A.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 using namespace std;
-struct Activity {
+struct Activity{
     int start, finish, index;
 };
 bool activityCompare(Activity a, Activity b){
     return a.finish < b.finish;
 }
-int main() {
+int main(){
     int n;
-    cout << "Enter number of activities: \n";
-    cin >> n;
+    cout<<"Enter number of activities: \n";
+    cin>>n;
     Activity activities[100]; 
-    cout << "Enter start and finish times of activities:\n";
+    cout<< "Enter start and finish times of activities:\n";
     for (int i = 0; i < n; i++) {
-        cin >> activities[i].start >> activities[i].finish;
+        cin>>activities[i].start>>activities[i].finish;
         activities[i].index = i + 1; 
     }
+    clock_t start_time = clock();
     sort(activities, activities + n, activityCompare);
     int S[100], k = 0;  
     S[k++] = activities[0].index;
     int previous = 0;
-    for (int current = 1; current < n; current++) {
+    for (int current =1;current < n;current++) {
         if (activities[current].start >= activities[previous].finish) {
             S[k++] = activities[current].index;
             previous = current;
         } else {
-            cout << "Activity " << activities[current].index << " is not possible\n";
+            cout<< "Activity " << activities[current].index<< " is not possible\n";
         }
     }
-    cout << "\nOptimal set of activities: ";
-    for (int i = 0; i < k; i++) {
-        cout << "A" << S[i] << " ";
+    cout<< "\nOptimal set of activities: ";
+    for (int i = 0; i < k; i++){
+        cout<< "A" << S[i]<< " ";
     }
-    cout << endl;
+    cout<<endl;
+    clock_t end_time = clock();
+    double execution_time = double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<< "\nExecution time: " << execution_time << " seconds" << endl;
     return 0;
 }
+
